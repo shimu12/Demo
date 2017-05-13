2e2baaef265b84de7d2b92e3b7f065912d83bf74
shimu12 <sww345@yeah.net>
2017-05-13 13:46:21 +0800

    测试

---
+++ src/com/test/common/TestJava.java
@@ -0,0 +1,11 @@
+package com.test.common;
+
+/**
+ * Created by shiweiwei on 2017/5/13.
+ */
+public class TestJava {
+
+    public static void main(String[] args){
+        System.out.println("1111");
+    }
+}
